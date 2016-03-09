e458dd9f0258d9e376257ba53754e819a40a2f45
deosaur <saurabhdeo27@gmail.com>
2016-03-09 06:02:30 -0800

    Create DiskMultiMap.cpp

---
+++ DiskMultiMap.cpp
@@ -0,0 +1,259 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <functional>
+#include "DiskMultiMap.h"
+
+using namespace std;
+
+// ------------------------------------------- DISK MULTI MAP CONSTRUCTOR -------------------------------------------------
+
+DiskMultiMap::DiskMultiMap() {}
+
+// ------------------------------------------- DISK MULTI MAP DESTRUCTOR -------------------------------------------------
+
+DiskMultiMap::~DiskMultiMap() {
+    close();
+}
+
+// -------------------------------------------------- CREATE NEW ---------------------------------------------------------
+
+bool DiskMultiMap::createNew(const std::string& filename, unsigned int numBuckets) {
+    m_numBuckets = numBuckets;
+    bf.write(m_numBuckets, 0);
+    
+    bool create = 1;
+    if(!openExisting(filename)) {       // BE WEARY OF THIS CODE!!!
+        create = bf.createNew(filename);
+    }
+    if(!create) {
+        cout << "Error Creating New Binary File!" << endl;
+        return false;
+    }
+    
+    r_head = 0;
+    bool reuse = bf.write(r_head, sizeof(m_numBuckets));
+    
+    if(!reuse) {
+        cout << "Error Creating Reuse Head!" << endl;
+        return false;
+    }
+    
+    for(int i = 0; i < m_numBuckets; i++) {  // set all the num buckets
+        Bucket new_buck(0);
+        bool check = bf.write(new_buck, sizeof(m_numBuckets) + m_sizeOffset + m_sizeBucket*i);
+        if(!check) {
+            cout << "Error writing a bucket!" << endl;
+            return false;
+        }
+    }
+    
+    m_sizeOfBuckArray = m_sizeBucket*m_numBuckets;
+    
+    return true;
+}
+
+// -------------------------------------------------- OPEN EXISTING -------------------------------------------------------
+
+bool DiskMultiMap::openExisting(const std::string& filename) {
+    if(bf.isOpen())
+        bf.close();
+    return bf.openExisting(filename);
+}
+
+// ---------------------------------------------------- CLOSE -------------------------------------------------------------
+
+void DiskMultiMap::close() {
+    if(bf.isOpen())
+        bf.close();
+}
+
+// ---------------------------------------------------- INSERT -------------------------------------------------------------
+
+bool DiskMultiMap::insert(const std::string &key, const std::string &value, const std::string &context) {
+    
+    if(key.size() > 120 || value.size() > 120 || context.size() > 120) {
+        return false;
+    }
+    
+    hash<string> str_hash;
+    unsigned long hashValue = str_hash(key);
+    unsigned int bucket = hashValue % m_numBuckets;
+    BinaryFile::Offset bucket_addr = m_sizeOfHeader + bucket*m_sizeBucket;
+    
+    Bucket buck(0);
+    bool buck_read = bf.read(buck, bucket_addr);
+    if(!buck_read) {
+        cout << "Error Reading Bucket!" << endl;
+        return false;
+    }
+    
+    HashNode new_hash(key, value, context, 0);
+    
+    
+    if(r_head == 0) {  // no reusable nodes available I have to expand the memory
+        if(buck.m_bucketOff == 0) { // if the bucket is not pointing to anything right now
+            buck.m_bucketOff = bf.fileLength();
+            bf.write(buck, bucket_addr);
+            bf.write(new_hash, buck.m_bucketOff);
+        }
+        
+        else {
+            new_hash.m_next = buck.m_bucketOff;
+            buck.m_bucketOff = bf.fileLength();
+            bf.write(buck, bucket_addr);
+            bf.write(new_hash, buck.m_bucketOff);
+        }
+    }
+    
+    else {  // we have a reusable node and must use it!
+        if(buck.m_bucketOff == 0) {
+            HashNode temp(" ", " ", " ", 0);
+            bf.read(temp, r_head);
+            buck.m_bucketOff = r_head;
+            bf.write(buck, bucket_addr);
+            bf.write(new_hash, buck.m_bucketOff);
+            r_head = temp.m_next;
+        }
+        
+        else {
+            HashNode temp(" ", " ", " ", 0);
+            bf.read(temp, r_head);
+            new_hash.m_next = buck.m_bucketOff;
+            buck.m_bucketOff = r_head;
+            bf.write(buck, bucket_addr);
+            bf.write(new_hash, buck.m_bucketOff);
+            r_head = temp.m_next;
+        }
+    }
+    
+    return true;
+}
+
+// ------------------------------------------------- ITERATOR SEARCH ----------------------------------------------------------
+/*
+DiskMultiMap::Iterator DiskMultiMap::search(const std::string &key) {
+     // PLACEHOLDER CODE
+}
+*/
+// ---------------------------------------------------- ERASE -------------------------------------------------------------
+
+int DiskMultiMap::erase(const std::string &key, const std::string &value, const std::string &context) {
+    
+    hash<string> str_hash;
+    unsigned long hashValue = str_hash(key);
+    unsigned int bucket = hashValue % m_numBuckets;
+    BinaryFile::Offset bucket_addr = m_sizeOfHeader + bucket*m_sizeBucket;
+    
+    int removed = 0;
+    
+    Bucket buck(0);
+    bf.read(buck, bucket_addr);
+    //HashNode temp(" ", " ", " ", 0);
+    //bf.read(temp, buck.m_bucketOff);
+    
+    BinaryFile::Offset temp_bucketOff = buck.m_bucketOff;
+    BinaryFile::Offset prev_off = bucket_addr;
+    
+    while(temp_bucketOff != 0) {
+        
+        HashNode temp(" ", " ", " ", 0);
+        bf.read(temp, temp_bucketOff);
+        
+        if(temp.m_key == key.c_str() && temp.m_value == value.c_str() && temp.m_context == context.c_str()) {
+            
+            if(temp_bucketOff == buck.m_bucketOff) {  // we are removing the head node
+            
+                buck.m_bucketOff = temp.m_next;
+                bf.write(buck, bucket_addr);
+                BinaryFile::Offset holder = temp_bucketOff;
+                temp.m_next = 0;
+                
+                if(r_head == 0)
+                    r_head = holder;
+                else {
+                    BinaryFile::Offset point = r_head;
+                    r_head = holder;
+                    temp.m_next = point;
+                }
+                bf.write(r_head, sizeof(m_numBuckets));
+                bf.write(temp, temp_bucketOff);
+                temp_bucketOff = buck.m_bucketOff;
+                removed++;
+                continue;
+            }
+            
+            else {  // we are removing some middle or end node
+                
+                HashNode prev(" ", " ", " ", 0);
+                bf.read(prev, prev_off);
+                BinaryFile::Offset holder = temp_bucketOff;
+                prev.m_next = temp.m_next;
+                bf.write(prev, prev_off);
+                
+                if(r_head == 0)
+                    r_head = holder;
+                else {
+                    BinaryFile::Offset point = r_head;
+                    r_head = holder;
+                    temp.m_next = point;
+                }
+                
+                temp_bucketOff = prev.m_next;
+                removed++;
+                continue;
+            }
+            
+        }
+        
+        prev_off = temp_bucketOff;
+        temp_bucketOff = temp.m_next;
+        
+    }
+    
+    
+    return removed;
+}
+
+// ------------------------------------ ITERATOR FUNCTIONS BEGIN -------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------------
+
+DiskMultiMap::Iterator::Iterator() {
+    
+}
+
+
+// ------------------------------------- DELETE THIS -------------------------------------------
+
+void DiskMultiMap::print()
+{
+    cout << "------------------" << endl;
+    cout << "NumBuckets: " << m_numBuckets << endl;
+    cout << "r_head: " << r_head << endl;
+    cout << "Filelength: " << bf.fileLength() << endl;
+    cout << "Size of offset: " << m_sizeOffset << endl;
+    cout << "Size of bucket: " << m_sizeBucket << endl;
+    cout << "Size of node: " << m_sizeNode << endl;
+    cout << "-------------------" << endl;
+    for (int i = 0; i < m_numBuckets; i++)
+    {
+        Bucket b(0);
+        bf.read(b, sizeof(m_numBuckets) + m_sizeOffset + (i*m_sizeBucket));
+        cout << "bucket address: " << sizeof(m_numBuckets) + m_sizeOffset + (i*m_sizeBucket) << endl;
+        cout << "bucket's node address: " << b.m_bucketOff << endl;
+    }
+    cout << "-----------------" << endl;
+    for (int i = 0; i < 8; i++)
+    {
+        HashNode n(" ", " ", " ", 0);
+        bf.read(n, m_sizeOfHeader + m_sizeOfBuckArray + i*m_sizeNode);					// CHECK VALUE
+        cout << "Address: " << m_sizeOfHeader + m_sizeOfBuckArray + i * m_sizeNode << endl;
+        cout << "Key: " << n.m_key << endl;
+        cout << "Value: " << n.m_value << endl;
+        cout << "Context: "<< n.m_context << endl;
+        cout << "Next: " << n.m_next << endl;
+        cout << endl;
+    }
+
+}
+
