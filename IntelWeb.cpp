68b369227373f4afb0f54ee83734e5ff7a588456
deosaur <saurabhdeo27@gmail.com>
2016-03-10 17:11:32 -0800

    Update IntelWeb.cpp

--- IntelWeb.cpp
+++ IntelWeb.cpp
@@ -3,6 +3,9 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <queue>
+#include <unordered_map>
+#include <set>
 #include "IntelWeb.h"
 #include "InteractionTuple.h"
 
@@ -94,19 +97,62 @@ bool IntelWeb::ingest(const std::string& telemetryFile) {
 unsigned int IntelWeb::crawl(const std::vector<std::string>& indicators, unsigned int minPrevalenceToBeGood, std::vector<std::string>& badEntitiesFound, std::vector<InteractionTuple>& badInteractions) {
     
     // clear badentities and interactions vectors
+    badEntitiesFound.clear();
+    badInteractions.clear();
+    
+    queue<std::string> badqueue;
+    unordered_map<string, DiskMultiMap::Iterator> toIter;
+    unordered_map<string, DiskMultiMap::Iterator> fromIter;
+    unordered_map<string, bool> prevCache;
+    set<string> bad_entities;
+    set<InteractionTuple> interact_entities;
     
     // push all the current indicators ones onto a LOCAL QUEUE
     
-    // take front of the queue and search m_From hashmap
+      for(int i = 0; i < indicators.size(); i++) {
+        badqueue.push(indicators.at(i));
+      }
     
+    // take front of the queue and search m_From hashmap
+    while(!badqueue.empty()) {
+        string temp_front = badqueue.front();
+        badqueue.pop();
+        DiskMultiMap::Iterator Iter_From = m_From.search(temp_front);
+        DiskMultiMap::Iterator Iter_To = m_To.search(temp_front);
+        
+        if(!Iter_From.isValid() && !Iter_To.isValid())
+            continue;
+        
     // de-reference the iterator and check the value in the tuple
+        MultiMapTuple m_from = *Iter_From;
+        // check if value is in the map
+        bool checkPrev = countPrev(m_from.value, minPrevalenceToBeGood);
+        
+        
+    
+    // 2 more unordered maps to track iterators for from and to
+    
+    
+    // LOCAL UNORDERED MAP (HASH TABLE) HOLDS VALIDITY OF YOUR PREVALANCE (VALID IF BELOW THRESHOLD, INVALID IF ABOVE)
     
     // check the prevalance of the value taken (write function in private to do this)
             // if its low enough, move the iterator, store value onto queue, store into LOCAL unordered map (checkprev does)
             // if its high enough, move the iterator along but dont push onto queue
     
+    } // end if crawl while loop
     
-    return 0; // PLACEHOLDER CODE
+    // copy the bad entities set to the vector (increasing order for the vector)
+    for(auto p = bad_entities.begin(); p != bad_entities.end(); p++) {
+        badEntitiesFound.push_back(*p);
+    }
+    
+    // copy the interaction tuples set to the vector (increasing order for the vector)
+    for(auto j = interact_entities.begin(); j != interact_entities.end(); j++) {
+        badInteractions.push_back(*j);
+    }
+    
+    // type cast and return
+    return (unsigned int)badEntitiesFound.size(); // PLACEHOLDER CODE
 }
 
 // ----------------------------------------------- PURGE ------------------------------------------------------------------
@@ -123,9 +169,10 @@ bool IntelWeb::purge(const std::string& entity) {  // MIGHT not be in the correc
         string value = m.value;
         string cont = m.context;
         
+        ++it;
         m_From.erase(key, value, cont);
         m_To.erase(value, key, cont);
-        ++it;
+        //++it;
         purged = 1;
         
     }
@@ -139,9 +186,10 @@ bool IntelWeb::purge(const std::string& entity) {  // MIGHT not be in the correc
         string value = m.value;
         string cont = m.context;
         
+        ++it2;
         m_To.erase(key, value, cont);
         m_From.erase(value, key, cont);
-        ++it2;
+        //++it2;
         purged = 1;
         
     }
